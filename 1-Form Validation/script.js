c17043828fd66276950178254e55aea5240908d0
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-04-14 20:32:16 +0200

    Javascript Projects

---
+++ 1-Form Validation/script.js
@@ -0,0 +1,5 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+const email = document.getElementById('email');
